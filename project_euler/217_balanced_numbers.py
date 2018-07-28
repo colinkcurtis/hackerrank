3ed2c1634958cf321efa5bf6adff67e282af3477
Colin@Home <colinkcurtis@gmail.com>
2018-07-28 17:45:23 -0400

    basic input handling done

--- project_euler/217_balanced_numbers.py
+++ project_euler/217_balanced_numbers.py
@@ -1,4 +1,17 @@
-input1 = input()
+input_line_1 = input()
+input_1_values = [int(x) for x in input_line_1.split(' ')]
 
+input_line_2 = input()
+input_2_values = [int(x) for x in input_line_2.split(' ')]
 
-print (input1)
+base = input_1_values[0]
+
+number_of_integers = input_1_values[1]
+
+integers = input_2_values
+
+print('base:', base)
+
+print('number_of_integers:', number_of_integers)
+
+print('integer list:', integers)
