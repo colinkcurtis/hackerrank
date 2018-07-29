9e2de77fabb4f3476c83acaf4376f26a7e397328
Colin@Home <colinkcurtis@gmail.com>
2018-07-28 20:23:03 -0400

    conversion from input_digits to N, complete

--- project_euler/217_balanced_numbers.py
+++ project_euler/217_balanced_numbers.py
@@ -5,21 +5,22 @@ input_line_2 = input()
 input_2_values = [int(x) for x in input_line_2.split(' ')]
 
 base = input_1_values[0]
-
 N = input_1_values[1]
 
 input_digits = input_2_values
 
 powers_list = list(reversed(range(N)))
 
-print('base:', base)
+# print('powers_list', powers_list)
 
-print('number_of_integers:', N)
+# print('base:', base)
 
-print('integer list:', input_digits)
+# print('number_of_integers:', N)
 
-print('powers list:', powers_list)
+# print('input digits:', input_digits)
 
-#converted_digits = [base**]
+# print('powers list:', powers_list)
 
-#print(N)
+converted_digits = [(base**powers_list[x])*input_digits[x] for x in powers_list if input_digits[x]]
+N = sum(converted_digits)
+print('N:', N)
