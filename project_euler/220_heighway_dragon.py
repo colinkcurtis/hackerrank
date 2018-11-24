03046b2f5bb8803a783afbeb18d66cf72a621c4c
Colin Curtis <ckcurtis@renci.org>
2018-11-23 23:55:40 -0500

    method for converting m to decimal completed

--- project_euler/220_heighway_dragon.py
+++ project_euler/220_heighway_dragon.py
@@ -15,10 +15,13 @@ class HeighweyDragon():
         query_list = [(inputs[i], inputs[i+1]) for i in range(1, 2*num_queries_in_file, 2)]
         return query_list
             
-    def 
-            
-    
+    def decimal_query_list(self, query_list):
+        decimal_query_list = [(x[0], int(x[1], base=16)) for x in query_list]
+        return decimal_query_list
+        
 if fileinput.input():
     HeighweyDragonObject = HeighweyDragon()
     inputs = HeighweyDragonObject.getinput(fileinput.input())
     query_list = HeighweyDragonObject.make_query_list(inputs)
+    decimal_query_list = HeighweyDragonObject.decimal_query_list(query_list)
+    print(decimal_query_list)
