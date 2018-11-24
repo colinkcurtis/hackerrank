4962b33f7d8cec7c832b564d669c64b0287ecc1c
Colin Curtis <ckcurtis@renci.org>
2018-11-24 00:30:13 -0500

    added function to find the Heighwey Dragon of size 'n', D_n

--- project_euler/220_heighway_dragon.py
+++ project_euler/220_heighway_dragon.py
@@ -1,13 +1,12 @@
 import fileinput
+import asyncio
 
 class HeighweyDragon():
     def __init__(self):
         pass
     
     def getinput(self, input):
-        input_list = []
-        for line in input:
-            input_list.append(line.rstrip())
+        input_list = [line.rstrip() for line in input]
         return input_list
     
     def make_query_list(self, inputs):
@@ -18,10 +17,29 @@ class HeighweyDragon():
     def decimal_query_list(self, query_list):
         decimal_query_list = [(x[0], int(x[1], base=16)) for x in query_list]
         return decimal_query_list
-        
+
+    def make_heighwey_dragon(self, pair):
+        a_map = 'aRbFR'
+        b_map = 'LFaLb'
+        d_o = 'Fa'
+        n = pair[0]
+        heighwey_dragon = []
+        heighwey_dragon.append(d_o)
+        for i in range(int(n)):
+            d_i = d_o.replace('a',a_map).replace('b', b_map)
+            heighwey_dragon.append(d_i)
+            d_o = d_i
+        return(heighwey_dragon)
+    
 if fileinput.input():
     HeighweyDragonObject = HeighweyDragon()
+    
     inputs = HeighweyDragonObject.getinput(fileinput.input())
+    
     query_list = HeighweyDragonObject.make_query_list(inputs)
+    
     decimal_query_list = HeighweyDragonObject.decimal_query_list(query_list)
-    print(decimal_query_list)
+    
+    make_heighwey_dragon = HeighweyDragonObject.make_heighwey_dragon(decimal_query_list[0])
+    
+    print(make_heighwey_dragon)
