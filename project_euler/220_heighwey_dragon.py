2ceb44930563b0a916d816baa68969775fcba8ee
Colin Curtis <ckcurtis@renci.org>
2018-11-25 02:44:38 -0500

    implemented new method for creating the dragon instructions

--- project_euler/220_heighwey_dragon.py
+++ project_euler/220_heighwey_dragon.py
@@ -1,7 +1,6 @@
 import fileinput
 import time
 import math
-#start = time.time()
 
 class HeighweyDragon():
     def __init__(self):
@@ -22,76 +21,71 @@ class HeighweyDragon():
         decimal_query_list = [(x[0], int(x[1], base=16)) for x in query_list]
         return decimal_query_list
 
-    def make_heighwey_dragon(self, pair):
-        h_start = time.time()
-        a_map = 'aRbFR'
-        b_map = 'LFaLb'
-        d_o = 'Fa'
-        n = pair[0]
-        heighwey_dragon = []
-        heighwey_dragon.append(d_o)
-
-        #d_n =
-        for i in range(int(n)):
-            d_list = []
-            seq_list = []
-            for index, x in enumerate(d_o):
-                if x in ('a'):
-                    #if index < 100:
-                        #print('a_index:', index)
-                    d_list.append(a_map)
-                elif x in ('b'):
-                    #if index < 100:
-                        #print('b_index:', index)
-                    d_list.append(b_map)
-                else:
-                    d_list.append(x)
-                    if index < 200:
-                        if x in ('L'):
-                            print('L_index:', index)
-                            seq_list.append(index)
-                        # elif x in ('R'):
-                        #     print('R_index:', index)
-                        # elif x in ('L'):
-                        #     print('L_index:', index)
-            d_list = ''.join(d_list)
-            heighwey_dragon.append(d_list)
-            d_o = d_list
-            if i == 3:
-                print(heighwey_dragon)
-        h_end = time.time()
-        
-        print('value of n, for D_n:', n)
-        print('time to create D_n:',h_end-h_start)
-        print()
-        print(seq_list)
-        return(heighwey_dragon[-1])
+    # def make_heighwey_dragon(self, pair):
+    #     h_start = time.time()
+    #     a_map = 'aRbFR'
+    #     b_map = 'LFaLb'
+    #     d_o = 'Fa'
+    #     n = pair[0]
+    #     heighwey_dragon = []
+    #     heighwey_dragon.append(d_o)
+    #     for i in range(int(n)):
+    #         d_list = []
+    #         seq_list = []
+    #         for index, x in enumerate(d_o):
+    #             if x in ('a'):
+    #                 d_list.append(a_map)
+    #             elif x in ('b'):
+    #                 d_list.append(b_map)
+    #             else:
+    #                 d_list.append(x)
+    #                 if index < 200:
+    #                     if x in ('L'):
+    #                         seq_list.append(index)
+    #         d_list = ''.join(d_list)
+    #         heighwey_dragon.append(d_list)
+    #         d_o = d_list
+    #         #if i == 3:
+    #             #print(heighwey_dragon)
+    #     h_end = time.time()
+    #     #print('old method time:', h_end-h_start)
+    #     return(heighwey_dragon[-1], seq_list)
     
     def make_heighwey_dragon_by_sequences(self, pair):
+        #STILL THE SLOWEST PART
         h_start = time.time()
-       
         d_o = 'Fa'
         n = pair[0]
-        heighwey_dragon = []
-        heighwey_dragon.append(d_o)
-
-
+        for i in range(int(n)):
+            string_0_mod = d_o[::-1]
+            string_0_mod_list = list(string_0_mod)
+            for index, letter in enumerate(string_0_mod_list):
+                if letter in ('b'):
+                    string_0_mod_list[index] = 'a'
+                    continue
+                elif letter in ('a'):
+                    string_0_mod_list[index] = 'b'
+                    continue
+                elif letter in ('L'):
+                    string_0_mod_list[index] = 'R'
+                    continue
+                elif letter in ('R'):
+                    string_0_mod_list[index] = 'L'
+                    continue
+                else:
+                    continue
+            flipped_reversed_string_0 = ''.join(string_0_mod_list)
+            d_n = d_o+'R'+flipped_reversed_string_0+'R'
+            d_o = d_n
+        h_end = time.time()
+        print('new method time:', h_end-h_start)
+        return d_n
 
     def rotation_matrix(self, theta):
         A = round(math.cos(theta))
         B = round(-math.sin(theta))
         C = round(math.sin(theta))
         D = round(math.cos(theta))
-        # if theta == 'left':
-        #     A = 0
-        #     B = -1
-        #     C = 1
-        #     D = 0
-        # elif theta == 'right':
-        #     A = 0
-        #     B = 1
-        #     C = -1
-        #     D = 0
         rotation_matrix=[(A,B),(C,D)]
         return rotation_matrix
     
@@ -100,24 +94,6 @@ class HeighweyDragon():
         new_x = rotation_matrix[0][0]*orientation[0]+rotation_matrix[0][1]*orientation[1]
         new_y = rotation_matrix[1][0]*orientation[0]+rotation_matrix[1][1]*orientation[1]
         new_orientation = (new_x, new_y)
-        # if theta in ('left'):
-        #     if orientation == (0,1):
-        #         new_orientation = (-1,0)
-        #     elif orientation == (-1,0):
-        #         new_orientation = (0,-1)
-        #     elif orientation == (0,-1):
-        #         new_orientation = (1,0)
-        #     elif orientation == (1,0):
-        #         new_orientation = (0,1)
-        # else:
-        #     if orientation == (0,1):
-        #         new_orientation = (1,0)
-        #     elif orientation == (1,0):
-        #         new_orientation = (0,-1)
-        #     elif orientation == (0,-1):
-        #         new_orientation = (-1,0)
-        #     elif orientation == (-1,0):
-        #         new_orientation = (0,1) 
         return new_orientation
     
     def new_position(self, position, orientation):
@@ -126,21 +102,18 @@ class HeighweyDragon():
                         
     def move_cursor(self, decimal_query_list):
         output = []
-        #theta_L = 'left'
         theta_L = math.pi/2
-        #theta_R = 'right'
         theta_R = -math.pi/2
         j=1
         for pair in decimal_query_list:
-            print('loop #:', j)
             j+=1
-            heighwey_dragon = self.make_heighwey_dragon(pair)
+            heighwey_dragon_seq = self.make_heighwey_dragon_by_sequences(pair)
             position = (0,0)
             orientation = (0,1)
             steps = pair[1]
             i = 0
             while i < steps:
-                for x in heighwey_dragon:
+                for x in heighwey_dragon_seq:
                     if i >= steps:
                         break
                     if x in ('F'):
@@ -155,6 +128,7 @@ class HeighweyDragon():
                     else:
                         orientation = self.rotate_orientation(theta_R, orientation)  
             output.append(position)
+            end = time.time()
         return output
               
 # if fileinput.input():
@@ -173,9 +147,7 @@ if __name__ == '__main__':
     query_list = HeighweyDragonObject.make_query_list(inputs)
     decimal_query_list = HeighweyDragonObject.decimal_query_list(query_list)
     moved_cursor = HeighweyDragonObject.move_cursor(decimal_query_list)
+    
     for x in moved_cursor:
         for y in x:
             print(hex(y).replace('0x','').upper())
-    
-    #end = time.time()
-    #print(end-start)
